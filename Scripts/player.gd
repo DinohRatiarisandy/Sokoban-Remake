92386c3273dd4fdf09a0a6f6a8702f9060481d1b
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-02-14 21:44:37 +0300

    add some songs

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,6 +3,9 @@ class_name Player
 extends CharacterBody2D
 
 @onready var ray_cast_2d = $RayCast2D
+@onready var hurt_sound = $SoundManager/HurtSound
+@onready var slide = $SoundManager/Slide
+@onready var move_sound = $SoundManager/MoveSound
 
 var inputs = {"move_right": Vector2.RIGHT, "move_left": Vector2.LEFT, "move_up": Vector2.UP, "move_down": Vector2.DOWN}
 var stop_movement : bool = false;
@@ -26,11 +29,14 @@ func move(dir):
 	# if player is not collide with anything, he can move
 	if !collider:
 		animate_movement(self.position + new_pos)
-		
+		move_sound.play()
 	else:
 		if collider is Crate and collider.can_move(dir):
 			collider.animate_movement(collider.position + new_pos)
 			animate_movement(self.position + new_pos)
+			slide.play()
+		else:
+			hurt_sound.play()
 
 
 func animate_movement(new_pos: Vector2):
--- Scripts/target.gd
+++ Scripts/target.gd
@@ -2,9 +2,12 @@ extends Area2D
 
 var occupied : bool = false;
 
+@onready var crate_in_place_sound = $CrateInPlaceSound
+
 func _on_body_entered(_body):
-	occupied = true;
+	occupied = true
+	crate_in_place_sound.play()
 
 
 func _on_body_exited(_body):
-	occupied = false;
+	occupied = false
